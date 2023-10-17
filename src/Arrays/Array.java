24d035d55da36f2ac020a37edd3fac1371fa4606
Alberto Noguchi <alberto2611@gmail.com>
2023-10-17 17:54:06 -0300

    first commit

---
+++ src/Arrays/Array.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+
+        // estrutura de dados simples para armazenar uma coleção de elementos
+
+        // forma mais comum
+        int[] arrayDeInt;
+
+        // forma menos comum
+        int arrayDeInt02[];
+
+        // inicializando um array com um tamanho definido
+        int[] arrayDeInt03 = new int[20];
+
+        int[] arrayDeInt04 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    }
+}
