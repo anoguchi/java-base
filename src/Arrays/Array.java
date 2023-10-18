9af5a35554d1b79c09c0a709105b3713c79028b7
Alberto Noguchi <alberto2611@gmail.com>
2023-10-18 17:01:26 -0300

    add: Arrays

--- src/Arrays/Array.java
+++ src/Arrays/Array.java
@@ -1,5 +1,8 @@
 package Arrays;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Array {
     public static void main(String[] args) {
 
@@ -14,7 +17,22 @@ public class Array {
         // inicializando um array com um tamanho definido
         int[] arrayDeInt03 = new int[20];
 
-        int[] arrayDeInt04 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Integer[] arrayDeInt04 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        // percorrendo o array
+        List<Integer> itens = Arrays.asList(arrayDeInt04);
+        itens.forEach(System.out::println);
+
+        // fazendo cópias
+        String[] treinamento = new String[]{"Treinamento", "descomplicando", "Java", "e", "Spring", "da", "Linuxtips"};
+        String[] treinamentoNome = Arrays.copyOfRange(treinamento, 1, 5);
+        List<String> treinamentoNomeList = List.of(treinamentoNome);
+        treinamentoNomeList.forEach(System.out::println);
+
+        // fazendo cópias escolhendo o tamanho
+        String[] arraycom2 = Arrays.copyOf(treinamentoNome, 4);
+        List<String> arrayCom2List = List.of(arraycom2);
+        arrayCom2List.forEach(System.out::println);
 
     }
 }
