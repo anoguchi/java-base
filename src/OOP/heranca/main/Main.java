7ab9b271cc5aa2ce2e75f793e30b1fbd73295a34
Alberto Noguchi <alberto2611@gmail.com>
2023-12-04 19:54:57 -0300

    add: herança, upcasting e downcasting

---
+++ src/OOP/heranca/main/Main.java
@@ -0,0 +1,28 @@
+package OOP.heranca.main;
+
+/*
+ * É um tipo de associação que permite que uma classe herde todos os dados comportamentos de outra
+ * - Definições importantes
+ * - Vantagens
+ *   - Reuso
+ *   - Polimorfismo
+ * - Sintaxe
+ *   - class A extends B
+ *
+ * Relação "é-um"
+ * Generação/especialização
+ * Superclasse (classe base) / subclasse (classe derivada)
+ * Herança / extensão
+ * É uma associação entre classe (e não entre objetos)
+ *
+ * Suponha um negócio de banco que possui uma conta comum e uma conta para empresas, sendo que a conta para empresa
+ * possui todos membros da conta comum, mais um limite de empréstimo e uma operação de realizar empréstimo.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/OOP/heranca/upcastingAndDowncasting/Main.java
@@ -0,0 +1,40 @@
+package OOP.heranca.upcastingAndDowncasting;
+
+/*
+ * Casting da subclasse para superclass. Uso comum em polimorfismo.
+ */
+
+import OOP.heranca.upcastingAndDowncasting.entities.Account;
+import OOP.heranca.upcastingAndDowncasting.entities.BusinessAccount;
+import OOP.heranca.upcastingAndDowncasting.entities.SavingsAccount;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Account account = new Account(1001, "Beto", 0.0);
+        BusinessAccount businessAccount = new BusinessAccount(1002, "Julia", 0.0, 500.00);
+
+        // Upcasting
+        Account account1 = businessAccount;
+        Account account2 = new BusinessAccount(1003, "Alberto", 0.0, 200.00);
+        Account account3 = new SavingsAccount(1004, "Diego", 0.0, 0.01);
+
+        // Downcasting
+        BusinessAccount account4 = (BusinessAccount) account2;
+        account4.loan(100.0);
+
+        if (account3 instanceof BusinessAccount) {
+            BusinessAccount account5 = (BusinessAccount) account3;
+            account5.loan(200.0);
+            System.out.println("Loan!");
+        }
+
+        if (account3 instanceof SavingsAccount) {
+            SavingsAccount account5 = (SavingsAccount) account3;
+            account5.updateBalance();
+            System.out.println("Update!");
+        }
+
+
+    }
+}
