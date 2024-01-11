fbf858c35aa61d4bd9f162573cdec0bd3577715d
Alberto Noguchi <alberto2611@gmail.com>
2024-01-10 23:40:44 -0300

    add: playground

---
+++ src/playground/Jukebox1.java
@@ -0,0 +1,20 @@
+package playground;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Jukebox1 {
+    public static void main(String[] args) {
+        new Jukebox1().go();
+    }
+    public void go() {
+        List<String> songList = MockSongs.getSongStrings();
+        System.out.println(songList);
+        Collections.sort(songList);
+        System.out.println(songList);
+
+        List<SongV2> songV2List = MockSongs.getSongsV2();
+        System.out.println(songV2List);
+
+    }
+}
