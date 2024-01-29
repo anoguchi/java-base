d33ff306561760a1eeb5ae7ef00900df4a5bf518
Alberto Noguchi <alberto2611@gmail.com>
2024-01-28 21:48:49 -0300

    add: gui part 1

---
+++ src/playground/gui2/SimpleGui2.java
@@ -0,0 +1,32 @@
+package Playground.gui2;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SimpleGui2 implements ActionListener {
+    private JButton button;
+
+    public static void main(String[] args) {
+        SimpleGui2 gui = new SimpleGui2();
+        gui.go();
+    }
+
+    public void go() {
+        JFrame frame = new JFrame();
+        button = new JButton("Click me");
+        MyDrawPanel myDrawPanel = new MyDrawPanel();
+
+        button.addActionListener(this);
+
+        frame.getContentPane().add(button);
+        frame.getContentPane().add(myDrawPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 800);
+        frame.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent event) {
+        button.setText("I've been clicked!");
+    }
+}
---
+++ src/playground/twoButtons/TwoButtons.java
@@ -0,0 +1,17 @@
+package Playground.twoButtons;
+
+import javax.swing.*;
+
+public class TwoButtons {
+    private JFrame frame;
+    private JLabel label;
+
+    public static void main(String[] args) {
+        TwoButtons gui = new TwoButtons();
+        gui.go();
+    }
+
+    public void go() {
+
+    }
+}
