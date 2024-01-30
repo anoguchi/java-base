99797352cbb318ddceb9bf23533f01e2f257b7aa
Alberto Noguchi <alberto2611@gmail.com>
2024-01-29 23:00:45 -0300

    add: gui part 2

--- src/playground/twoButtons/TwoButtons.java
+++ src/playground/twoButtons/TwoButtons.java
@@ -1,6 +1,9 @@
 package Playground.twoButtons;
 
+import Playground.gui3.MyDrawPanel;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class TwoButtons {
     private JFrame frame;
@@ -12,6 +15,38 @@ public class TwoButtons {
     }
 
     public void go() {
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        JButton labelButton = new JButton("Change Label");
+        //labelButton.addActionListener(new LabelListener());
+        labelButton.addActionListener(event -> label.setText("Ouch!"));
+
+        JButton colorButton = new JButton("Change Circle");
+        //colorButton.addActionListener(new ColorListener());
+        colorButton.addActionListener(event -> frame.repaint());
+
+        label = new JLabel("Im a label");
+        MyDrawPanel drawPanel = new MyDrawPanel();
+
+        frame.getContentPane().add(BorderLayout.SOUTH, colorButton);
+        frame.getContentPane().add(BorderLayout.CENTER, drawPanel);
+        frame.getContentPane().add(BorderLayout.EAST, labelButton);
+        frame.getContentPane().add(BorderLayout.WEST, label);
+
+        frame.setSize(500, 400);
+        frame.setVisible(true);
     }
+
+//    class LabelListener implements ActionListener {
+//        public void actionPerformed(ActionEvent event) {
+//            label.setText("Ouch!");
+//        }
+//    }
+//
+//    class ColorListener implements ActionListener {
+//        public void actionPerformed(ActionEvent event) {
+//            frame.repaint();
+//        }
+//    }
 }
